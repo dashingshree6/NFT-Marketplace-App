534b037d47e327d245aafd8155d99ee484e8c456
dashingshree6 <dashingshree6@gmail.com>
2022-06-11 19:02:18 +0530

    Create Home.js

---
+++ client-side/screens/Home.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import { View, SafeAreaView, FlatList } from "react-native";
+
+
+const Home = () => {
+    return (
+        <View>Home Page</View>        
+    )
+}
+export default Home;
