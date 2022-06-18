794b68b8bf6d58c82d0eded4779332a22c40a670
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-06-18 23:54:21 +0530

    Update dummy.js

--- client-side/constants/dummy.js
+++ client-side/constants/dummy.js
@@ -63,7 +63,7 @@ const NFTData = [
     bids: [
       {
         id: "BID-21",
-        name: "Jessica Tan",
+        name: "John Tan",
         price: 7.05,
         image: assets.person04,
         date: "December 12, 2019 at 12:10 PM",
