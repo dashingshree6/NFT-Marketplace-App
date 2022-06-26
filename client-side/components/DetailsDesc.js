a0bba715bbad89e144a52e34462e7ce39ef901ff
dashingshree6 <dashingshree6@gmail.com>
2022-06-26 23:39:23 +0530

    Update DetailsDesc.js

--- client-side/components/DetailsDesc.js
+++ client-side/components/DetailsDesc.js
@@ -3,3 +3,7 @@ import { View, Text } from "react-native";
 
 import { EthPrice, NFTTitle } from "./SubInfo";
 import { COLORS, SIZES, FONTS } from "../constants";
+
+const DetailsDesc = ({ data }) => {
+    const [text, setText] = useState(data.description.slice(0, 100));
+    const [readMore, setReadMore] = useState(false);
