ce804dde90c25ee6aace0193900925a9a4f094dd
dashingshree6 <dashingshree6@gmail.com>
2022-06-26 23:38:49 +0530

    Update DetailsDesc.js

--- client-side/components/DetailsDesc.js
+++ client-side/components/DetailsDesc.js
@@ -1,3 +1,5 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
 
+import { EthPrice, NFTTitle } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
