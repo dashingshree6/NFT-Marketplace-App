d76e27ba52b8faf9b4ac6124cad17132d006b756
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-06-29 21:09:33 +0530

    Update Home.js

--- client-side/screens/Home.js
+++ client-side/screens/Home.js
@@ -16,9 +16,44 @@ const Home = () => {
       item.name.toLowerCase().includes(value.toLowerCase())
     );
      
-          
-    return (
-        <View>Home Page </View>        
-    )
-}
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar backgroundColor={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+          />
+        </View>
+
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View
+            style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
 export default Home;
