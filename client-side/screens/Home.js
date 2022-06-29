007977d1d5ff10c01539e2bd095f3b9a014a973a
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-06-29 21:03:43 +0530

    Update Home.js

--- client-side/screens/Home.js
+++ client-side/screens/Home.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import { View, SafeAreaView, FlatList } from "react-native";
 
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+
 const Home = () => {
     return (
         <View>Home Page </View>        
