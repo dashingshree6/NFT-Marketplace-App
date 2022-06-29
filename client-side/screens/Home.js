e6029800c5ed37242159e852bcc2794be2d6b1b3
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-06-29 21:04:08 +0530

    Update Home.js

--- client-side/screens/Home.js
+++ client-side/screens/Home.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import { View, SafeAreaView, FlatList } from "react-native";
 
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
     return (
