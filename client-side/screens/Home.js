d23b7be8151e143458b72883465aa2567d710f36
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-06-29 21:06:49 +0530

    Update Home.js

--- client-side/screens/Home.js
+++ client-side/screens/Home.js
@@ -6,6 +6,12 @@ import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
      const [nftData, setNftData] = useState(NFTData);
+     
+     const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+          
     return (
         <View>Home Page </View>        
     )
