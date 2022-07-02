0ea25a4b47cb44e399e09554cf776abbe9de632d
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-02 22:37:09 +0530

    Update App.js

--- client-side/App.js
+++ client-side/App.js
@@ -35,7 +35,7 @@ const App = () => {
       initialRouteName="Home"
       >
         <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Details" component={Details}/>
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
