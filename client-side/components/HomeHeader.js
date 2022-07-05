9774edffe4db41c9a70eec59b108a443a48ab078
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-05 23:32:19 +0530

    Update HomeHeader.js

--- client-side/components/HomeHeader.js
+++ client-side/components/HomeHeader.js
@@ -23,7 +23,28 @@ const HomeHeader = ({ onSearch }) => {
            resizeMode="contain"
            style={{ width: 90, height: 25 }}
          />
+         
+         <View style={{ width: 45, height: 45 }}>
+           <Image
+             source={assets.person01}
+             resizeMode="contain"
+             style={{ width: "100%", height: "100%" }}
+           />
+            <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: "absolute",
+              width: 15,
+              height: 15,
+              bottom: 0,
+              right: 0,
+            }}
+          />
+         </View>
       </View>
+      
+      
     </View>
   )
 }
