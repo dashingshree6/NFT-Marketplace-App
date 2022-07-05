bd5cea607766ae2a581a3a950d905a45b6242eaf
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-05 23:24:29 +0530

    Update HomeHeader.js

--- client-side/components/HomeHeader.js
+++ client-side/components/HomeHeader.js
@@ -2,3 +2,15 @@ import React from "react";
 import { View, Text, Image, TextInput } from "react-native";
 
 import { COLORS, FONTS, SIZES, assets } from "../constants";
+
+const HomeHeader = ({ onSearch }) => {
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.fonts
+      }}
+    >
+    </View>
+  )
+}
