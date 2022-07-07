e4c8274ae84961294c85badb6db1d4b83721ce23
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-07 20:20:12 +0530

    Create NFTCard.js

---
+++ client-side/components/NFTCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { View, Image } from "react-native";
+
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
+import { RectButton, CircleButton } from "./Button";
