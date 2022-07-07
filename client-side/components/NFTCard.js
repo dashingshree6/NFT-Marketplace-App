1e6ab4f4759545fd8c3becbaf8e08dc39cf58e4f
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-07 20:20:54 +0530

    Update NFTCard.js

--- client-side/components/NFTCard.js
+++ client-side/components/NFTCard.js
@@ -5,3 +5,17 @@ import { View, Image } from "react-native";
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
 import { RectButton, CircleButton } from "./Button";
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
