cdddab5a0524fd20639a85111860e63076c1e01d
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-11 21:44:03 +0530

    Update Details.js

--- client-side/screens/Details.js
+++ client-side/screens/Details.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { View, SafeAreaView, FlatList } from "react-native";
 
+//Screen layout 1
 const Home = () => {
     return (
         <View>Details Page Layout</View>        
