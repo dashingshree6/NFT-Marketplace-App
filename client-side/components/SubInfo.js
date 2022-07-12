8f621fd780cc3229da759ba0159dba7f4724d2f5
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-12 23:56:48 +0530

    Update SubInfo.js

--- client-side/components/SubInfo.js
+++ client-side/components/SubInfo.js
@@ -5,20 +5,7 @@ import { SIZES, FONTS, COLORS, SHADOWS, assets } from "../constants";
 export const NFTTitle = ({ title, subTitle, titleSize, subTitleSize }) => {
     return (
         <View>
-            <Text
-            style={{
-                fontFamily: FONTS.semiBold,
-                fontSize: titleSize,
-                color: COLORS.primary
-            }}
-            >{title}</Text>
-            <Text
-            style={{
-                fontFamily: FONTS.regular,
-                fontSize: subTitleSize,
-                color: COLORS.primary
-            }}
-            >by {subTitle}</Text>
+            
         </View>
     )
 }
