d803c9116bd6c047a6720fd529e91fc4492c0294
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-07-13 23:39:06 +0530

    Update Details.js

--- client-side/screens/Details.js
+++ client-side/screens/Details.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
 
+import { COLORS, SIZES, assets, SHADOWS, FONTS } from "../constants";
+import { CircleButton, RectButton, SubInfo, DetailsDesc, DetailsBid, FocusedStatusBar } from "../components";
+
 //Screen layout 1
 const Home = () => {
     return (
